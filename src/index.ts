93afeef3628a21161da8d2fa1d58a049efbca61d
Daniele Guarneri <dguarneri71@outlook.com>
2021-10-15 18:43:40 +0200

    eliminate parti non funzionanti

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export { DgComponentsLibrary } from './libraries/dgComponents/DgComponentsLibrary';
-export {MyButton} from './libraries/dgComponents/components/myButton';
--- src/libraries/dgComponents/components/IMyButtonProps.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface IMyButtonProps {
-    clickHandler?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    doubleClickHandler?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    label: string;
-}
